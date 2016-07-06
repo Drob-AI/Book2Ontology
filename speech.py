15ef3896c3294c59ce3148316e18facacbe6327a
bladespinner <bladespinner@gmail.com>
2016-07-07 02:17:02 +0300

    Add speech name tagging

--- speech.py
+++ speech.py
@@ -11,7 +11,35 @@ def split_protospeech(proto_speech):
 def read_speech(data):
     whole_data = reduce(lambda a,b: a + b, data)
     speech_start = whole_data.split('\n"')
+    speeches = []
     for speech in speech_start:
-        print split_protospeech(speech)
+        speeches.append(split_protospeech(speech))
+    return speeches
 
-read_speech(read_hary_potter());
+def get_speech_names(name_speech, names):
+    words = name_speech.split(' ')
+    return set(words).intersection(set(names))
+
+
+def name_speech(data, names):
+    speeches = read_speech(data)
+    named_speeches = []
+    for speech in speeches:
+        speech_names = []
+        for token in speech:
+            if len(speech_names) > 0:
+                break
+            if (not token["isChar"]):
+                speech_names = list(get_speech_names(token["txt"], names))
+        name = None
+        if len(speech_names) > 0:
+            name = speech_names[0]
+        named_speeches.append({ "speeker": name, "speech_tokens": speech})
+    return named_speeches
+
+
+# read_speech(read_hary_potter());
+names = ["Harry", "Ron", "Hagrid"]
+
+for a in name_speech(read_hary_potter(), names):
+    print a
