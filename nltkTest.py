3b9470ebb034ba28f33eb7002d9d86a3169acbbe
Hristo Avramov <havramov@asteasolutions.com>
2016-07-06 17:07:30 +0300

    gender rec added

--- nltkTest.py
+++ nltkTest.py
@@ -1,7 +1,12 @@
 import nltk
+import genderRec
 # nltk.download()
 sentence = "At eight o'clock on Thursday morning"
 tokens = nltk.word_tokenize(sentence)
 print(tokens)
 tagged = nltk.pos_tag(tokens)
 print(tagged)
+
+clf = genderRec.clf
+genderRec.train_name_classifier(clf)
+print(genderRec.test_classifier(clf))
