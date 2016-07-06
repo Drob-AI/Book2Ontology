6c817a0524473c73cb0e32a3ceb81f8846cc0492
Hristo Avramov <havramov@asteasolutions.com>
2016-07-07 01:02:25 +0300

    fixes

--- nltkTest.py
+++ nltkTest.py
@@ -7,6 +7,6 @@ print(tokens)
 tagged = nltk.pos_tag(tokens)
 print(tagged)
 
-clf = genderRec.clf
-genderRec.train_name_classifier(clf)
-print(genderRec.test_classifier(clf))
+# clf = genderRec.clf
+# genderRec.train_name_classifier(clf)
+# print(genderRec.test_classifier(clf))
